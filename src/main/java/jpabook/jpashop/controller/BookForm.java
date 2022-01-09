598d551adb7d101ace8f7d5915ec45a3cacc71f7
Lemon Soju <yckk980320@naver.com>
2022-01-09 12:43:18 +0900

    Item Modification

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -5,12 +5,11 @@ import lombok.Setter;
 
 @Getter @Setter
 public class BookForm {
-    private Long id;
 
-    private String name;
-    private int price;
+    private Long id; // Alt + Shift + 마우스 좌클릭 -> 여러줄 동시에 입력
+    private String name; // Ctrl + Shift + 화살표 -> 여러줄 동시에 드래그
+    private int price; // Shift + Tab -> 여러줄 왼쪽으로 이동
     private int stockQuantity;
-
     private String author;
     private String isbn;
 }
